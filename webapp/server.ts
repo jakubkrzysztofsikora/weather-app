852d29747e98c9eaff40d18465af68739cdbd7f3
Jakub Sikora <jakub.sikora@enrouteq.com>
2024-03-22 15:16:05 +0100

    Corrected mistake with passing apiUrl to config object

--- webapp/server.ts
+++ webapp/server.ts
@@ -26,10 +26,7 @@ app.get("/config", (_req, res) => {
     process.env.VITE_API_URL ||
     "http://localhost:3001");
   const config: Config = {
-    apiUrl:
-      (process.env.API_URL ||
-      process.env.VITE_API_URL ||
-      "http://localhost:3001"),
+    apiUrl,
   };
   res.json(config);
 });
